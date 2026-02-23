8f0cb846ac42ceb634ab5a5850562a6e359b6fe4
agata <agata.fernanda4@gmail.com>
2026-02-23 10:27:40 -0300

    Cria métodos para buscar e-mail e verificar se um e-mail já existe

--- login/src/main/java/com/agata/login/repository/UserRepository.java
+++ login/src/main/java/com/agata/login/repository/UserRepository.java
@@ -2,4 +2,9 @@ package com.agata.login.repository;
 
 public interface UserRepository extends {
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
